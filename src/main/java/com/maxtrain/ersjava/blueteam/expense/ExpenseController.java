e0ec93984415a1e454f906b43bd74f00c46253ac
Jacob Witte <wittejg@gmail.com>
2023-04-04 15:33:36 -0400

    added expense and item controllers

--- src/main/java/com/maxtrain/ersjava/blueteam/expense/ExpenseController.java
+++ src/main/java/com/maxtrain/ersjava/blueteam/expense/ExpenseController.java
@@ -1,5 +1,61 @@
 package com.maxtrain.ersjava.blueteam.expense;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/expenses")
 public class ExpenseController {
 
+	@Autowired
+	private ExpenseRepository expRepo;
+	
+	@GetMapping
+	public ResponseEntity<Iterable<Expense>> getAllExpenses(){
+		Iterable<Expense> expenses = expRepo.findAll();
+		return new ResponseEntity<Iterable<Expense>>(expenses, HttpStatus.OK);
+	}
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Expense> getExpense(@PathVariable int id){
+		Optional<Expense> expense = expRepo.findById(id);
+		if(expense.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Expense>(expense.get(), HttpStatus.OK);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@PutMapping("{id}")
+	public ResponseEntity putExpense(@PathVariable int id, @RequestBody Expense expense){
+		if(expense.getId() != id) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		expRepo.save(expense);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+
+	
+	@PostMapping
+	public ResponseEntity<Expense> postExpense(@RequestBody Expense expense){
+		Expense newExpense = expRepo.save(expense);
+		return new ResponseEntity<Expense>(newExpense, HttpStatus.CREATED);
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@DeleteMapping
+	public ResponseEntity deleteExpense(@PathVariable int id) {
+		Optional<Expense> expense = expRepo.findById(id);
+		if(expense.isEmpty()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		expRepo.delete(expense.get());
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
 }
