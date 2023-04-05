0da9b6a461d0941e045dbb57fb843d63f7fe59bc
Jacob Witte <wittejg@gmail.com>
2023-04-05 14:28:42 -0400

    fix formatting

--- src/main/java/com/maxtrain/ersjava/blueteam/expense/ExpenseController.java
+++ src/main/java/com/maxtrain/ersjava/blueteam/expense/ExpenseController.java
@@ -1,20 +1,11 @@
 package com.maxtrain.ersjava.blueteam.expense;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-
-
-
-
 import com.maxtrain.ersjava.blueteam.employee.*;
-
-
-
 
 @CrossOrigin
 @RestController
